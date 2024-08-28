272b3d4ebf2cc0ecdba0772a260713e9c53dfa6c
GustavoDiniz17 <gustavodinizdemorais@gmail.com>
2024-08-28 12:33:19 -0300

    Connecting IA API Key

--- index.py
+++ index.py
@@ -4,3 +4,9 @@ import ffmpeg
 import openai
 from moviepy.editor import VideoFileClip
 from moviepy.video.io.ffmpeg_tools import ffmpeg_extract_audio
+import os
+from dotenv import load_dotenv
+
+
+load_dotenv()
+openai.api_key = os.getenv('API_TOKEN')
